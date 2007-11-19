ec4f258f272bc632689aa0a97f63e513dd4a922a
abreslav <abreslav@4c0c614a-373f-0410-97f1-c5b4fc1f5fa5>
2007-11-19 08:52:00 +0000

    Interfaces may represent classes for multiple inheritance

--- org.abreslav.java2ecore/src/org/abreslav/java2ecore/annotations/sfeatures/Ordered.java
+++ org.abreslav.java2ecore/src/org/abreslav/java2ecore/annotations/sfeatures/Ordered.java
@@ -1,9 +1,9 @@
-package org.abreslav.java2ecore.annotations;
+package org.abreslav.java2ecore.annotations.sfeatures;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
 @Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
-public @interface Unique {
+public @interface Ordered {
 
 }
--- org.abreslav.java2ecore/src/org/abreslav/java2ecore/annotations/sfeatures/Unsettable.java
+++ org.abreslav.java2ecore/src/org/abreslav/java2ecore/annotations/sfeatures/Unsettable.java
@@ -1,9 +1,9 @@
-package org.abreslav.java2ecore.annotations;
+package org.abreslav.java2ecore.annotations.sfeatures;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
 @Target(ElementType.FIELD)
-public @interface ID {
+public @interface Unsettable {
 
 }
