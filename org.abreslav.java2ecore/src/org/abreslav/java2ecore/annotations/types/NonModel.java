4fda317e5cf1ed6421babb80ae3437bd2b9161b8
abreslav <abreslav@4c0c614a-373f-0410-97f1-c5b4fc1f5fa5>
2007-11-24 19:38:34 +0000

    @NonModel supported Unknown superclass forbidden

---
+++ org.abreslav.java2ecore/src/org/abreslav/java2ecore/annotations/types/NonModel.java
@@ -0,0 +1,9 @@
+package org.abreslav.java2ecore.annotations.types;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+public @interface NonModel {
+
+}
