a33d9621bfc2bc4fc274a640c28605625350b2cb
abreslav <abreslav@4c0c614a-373f-0410-97f1-c5b4fc1f5fa5>
2007-11-26 20:29:51 +0000

    DefaultValueLiteral supported

---
+++ trunk/org.abreslav.java2ecore/src/org/abreslav/java2ecore/annotations/sfeatures/NoDefaultValue.java
@@ -0,0 +1,8 @@
+package org.abreslav.java2ecore.annotations.sfeatures;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+public @interface NoDefaultValue {
+}
