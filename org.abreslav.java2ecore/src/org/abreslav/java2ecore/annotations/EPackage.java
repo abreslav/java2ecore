226b76425a547d031d6d417f4d967fa1198c7bd0
abreslav <abreslav@4c0c614a-373f-0410-97f1-c5b4fc1f5fa5>
2007-11-28 21:08:37 +0000

    Some JavaDoc added and jardesc created

--- org.abreslav.java2ecore/src/org/abreslav/java2ecore/annotations/EPackage.java
+++ org.abreslav.java2ecore/src/org/abreslav/java2ecore/annotations/EPackage.java
@@ -3,8 +3,21 @@ package org.abreslav.java2ecore.annotations;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
+/**
+ * Denotes a class that describes EPackage
+ * @author abreslav
+ *
+ */
 @Target({ElementType.TYPE})
 public @interface EPackage {
+	
+	/**
+	 * Corresponds to nsPrefix attribute of an EPackage  
+	 */
 	String nsPrefix();
+
+	/**
+	 * Corresponds to nsURI attribute of an EPackage  
+	 */
 	String nsURI();
 }
