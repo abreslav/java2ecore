f4fce0521bcadd445665d8bb96f2ca43e838233c
abreslav <abreslav@4c0c614a-373f-0410-97f1-c5b4fc1f5fa5>
2007-11-30 10:09:00 +0000

    EPackage described in JavaDoc

--- org.abreslav.java2ecore/src/org/abreslav/java2ecore/annotations/EPackage.java
+++ org.abreslav.java2ecore/src/org/abreslav/java2ecore/annotations/EPackage.java
@@ -4,8 +4,39 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
 /**
- * Denotes a class that describes EPackage
- * @author abreslav
+ * Denotes a class (or interface) that describes EPackage.
+ * <br/>
+ * A class which is marked with this annotation is considered to be an EPackage 
+ * description. A class' name becomes EPackage's <code>name</code>, 
+ * <code>nsPrefix</code> and <code>nsURI</code> attributes are set through corresponding 
+ * annotation attributes. <br/>
+ * <br/>
+ * EPackage contents (i.e. classifiers and subpackages) are declared as nested classes. <br/>
+ * <br/>
+ * Example:
+ * <pre>
+ * &#064;EPackage(
+ *     nsPrefix = "example",
+ *     nsURI = "http:///example.com/example"
+ * )
+ * public interface example {
+ *     class ExampleClass {
+ *     
+ *     }
+ *     
+ *     &#064;EPackage(
+ *         nsPrefix = "sub",
+ *         nsURI = "http:///example.com/example/sub"
+ *     )
+ *     class sub {
+ *         class ClassInsideSubpackage {
+ *         }
+ *     }
+ * }
+ * </pre>
+ * 
+ * This annotation is mandatory for any top-level class in <code>ecores</code> 
+ * source folder.
  *
  */
 @Target({ElementType.TYPE})
