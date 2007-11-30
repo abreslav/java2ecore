9f9991e1b3d1b7c44a71db689e026c9bcb8a27be
abreslav <abreslav@4c0c614a-373f-0410-97f1-c5b4fc1f5fa5>
2007-11-30 20:53:08 +0000

    @EDataType documented

--- trunk/org.abreslav.java2ecore/src/org/abreslav/java2ecore/annotations/types/EDataType.java
+++ trunk/org.abreslav.java2ecore/src/org/abreslav/java2ecore/annotations/types/EDataType.java
@@ -3,10 +3,23 @@ package org.abreslav.java2ecore.annotations.types;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
+/**
+ * Denotes an EDataType with a specified instance type.<br/>
+ * <br/>
+ * Use for classes which represent EDataTypes. 
+ * Such classes cannot have any members or supertypes.
+ * <br/>
+ * Example:
+ * <pre>
+ * &#064;EDataType("javax.swing.JComponent")
+ * class SwingComponent { 
+ * }
+ * </pre>
+ */
 @Target(ElementType.TYPE)
 public @interface EDataType {
 	/**
-	 * Means this EDataType's instanceTypeName property value
+	 * Corresponds to EDataType's instanceTypeName property value
 	 */
 	String value();
 }
