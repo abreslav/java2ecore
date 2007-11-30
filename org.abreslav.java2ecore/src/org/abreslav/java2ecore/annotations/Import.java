f5d10cd2eb1af7eea38c0085dd16a230c72024be
abreslav <abreslav@4c0c614a-373f-0410-97f1-c5b4fc1f5fa5>
2007-11-30 20:46:45 +0000

    JavaDoc for imports (not implemented yet) and for @EClass

--- org.abreslav.java2ecore/src/org/abreslav/java2ecore/annotations/Import.java
+++ org.abreslav.java2ecore/src/org/abreslav/java2ecore/annotations/Import.java
@@ -3,6 +3,12 @@ package org.abreslav.java2ecore.annotations;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation allows you to use classes defined in another Java2Ecore
+ * source file.<br/>
+ *<br/>
+ * Not implemented yet.
+ */
 @Target({ElementType.TYPE})
 public @interface Import {
 	Class<?> value();
