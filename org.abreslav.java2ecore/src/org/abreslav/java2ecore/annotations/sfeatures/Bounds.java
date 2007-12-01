946d9881bd53d5ca33a799119dea20f1a9b1f923
abreslav <abreslav@4c0c614a-373f-0410-97f1-c5b4fc1f5fa5>
2007-12-01 12:48:54 +0000

    Comments for @Bounds @InstanceTypeName @NonModel

--- org.abreslav.java2ecore/src/org/abreslav/java2ecore/annotations/sfeatures/Bounds.java
+++ org.abreslav.java2ecore/src/org/abreslav/java2ecore/annotations/sfeatures/Bounds.java
@@ -3,7 +3,22 @@ package org.abreslav.java2ecore.annotations.sfeatures;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
+/**
+ * Allows to specify ETypedElement's bounds (lower and upper).<br/>
+ * <br/>
+ * Applicable for attributes, references, operations and parameters.<br/>
+ * One or two numbers have to be specified. If two numbers are specified (in 
+ * the form <code>&#064;Bounds({1, 5})</code>) then the first one is a lower bound 
+ * and the second is an upper bound. If only one number is specified 
+ * (<code>&#064;Bounds(1)</code>), then this number is both lower and upper bound.
+ *
+ */
 @Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
 public @interface Bounds {
+	/**
+	 * Corresponds to lowerBound and upperBound attributes.
+	 * 
+	 * @see Bounds
+	 */
 	int[] value() default {0, 1};
 }
--- org.abreslav.java2ecore/src/org/abreslav/java2ecore/annotations/types/NonModel.java
+++ org.abreslav.java2ecore/src/org/abreslav/java2ecore/annotations/types/NonModel.java
@@ -3,6 +3,13 @@ package org.abreslav.java2ecore.annotations.types;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
+/**
+ * Denotes a type which must be skipped by Java2Ecore.
+ * No Ecore classifier will be created from such a class.
+ * <br/>
+ * This annotation is not applicable to classes declared outside a package class. 
+ *
+ */
 @Target(ElementType.TYPE)
 public @interface NonModel {
 
