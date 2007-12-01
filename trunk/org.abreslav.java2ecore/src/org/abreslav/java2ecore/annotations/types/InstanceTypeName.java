3c8ea895bf5b30b901a71b700803b00d89396003
abreslav <abreslav@4c0c614a-373f-0410-97f1-c5b4fc1f5fa5>
2007-12-01 12:48:54 +0000

    Comments for @Bounds @InstanceTypeName @NonModel

--- trunk/org.abreslav.java2ecore/src/org/abreslav/java2ecore/annotations/types/InstanceTypeName.java
+++ trunk/org.abreslav.java2ecore/src/org/abreslav/java2ecore/annotations/types/InstanceTypeName.java
@@ -3,7 +3,16 @@ package org.abreslav.java2ecore.annotations.types;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
+/**
+ * Allows to set EClass' <code>instanceTypeName</code> property.
+ * <br/>
+ * Not applicable for EDataTypes. 
+ *
+ */
 @Target(ElementType.TYPE)
 public @interface InstanceTypeName {
+	/**
+	 * Corresponds to instanceTypeName attribute
+	 */
 	String value();
 }
