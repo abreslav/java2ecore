0140b7f0e6225f454d2a6bab50aca4cd26fce82e
abreslav <abreslav@4c0c614a-373f-0410-97f1-c5b4fc1f5fa5>
2007-12-07 10:04:00 +0000

    Path problem fixed

--- trunk/org.abreslav.java2ecore/src/org/abreslav/java2ecore/annotations/ImportGenModel.java
+++ trunk/org.abreslav.java2ecore/src/org/abreslav/java2ecore/annotations/ImportGenModel.java
@@ -7,13 +7,25 @@ import java.lang.annotation.Target;
  * This annotation allows you to use classes defined in some Ecore models from which
  * some Java code was generated.<br/>
  * <br/>
- * Specify current project-relative paths to *.genmodel resources
- * containing EMF GenModels for some Ecore models
+ * Specify paths to *.genmodel resources containing EMF GenModels for some Ecore models.
+ * <br/>
+ * You can specify either relative or absolute paths, but those two mean different things:
+ * <ul>
+ *   <li> If you specify a relative path (like "model/my.gemodel") then it is treated as 
+ *        current project relative: it is appended to the path of the project where
+ *        your Java2Ecore source file is located.
+ *   </li>
+ *   <li> If you specify an absolute path (like "/other.project/model/other.genmodel")
+ *        then it is treated as workspace-relative, so the first segment has to be
+ *        a project name. 
+ *   </li>
+ * </ul>
+ * NOTE: Paths with device specification (like "C:/somepath") are not supported.
  */
 @Target({ElementType.TYPE})
 public @interface ImportGenModel {
 	/**
-	 * An array of current project relative paths to *.genmodel resources
+	 * An array of paths to *.genmodel resources
 	 * containing EMF GenModels for some Ecore models
 	 * @return
 	 */
